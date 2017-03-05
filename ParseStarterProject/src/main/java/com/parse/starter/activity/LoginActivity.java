9257d6f5b6d3a3fb92a3998c129c1fe608577de1
RafaelNercessian <rafanercessian@gmail.com>
2017-03-05 19:08:02 -0300

    Activity cadastro

--- ParseStarterProject/src/main/java/com/parse/starter/activity/LoginActivity.java
+++ ParseStarterProject/src/main/java/com/parse/starter/activity/LoginActivity.java
@@ -1,17 +1,31 @@
 package com.parse.starter.activity;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import com.parse.starter.R;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private TextView cadastrar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        cadastrar=(TextView) findViewById(R.id.login_cadastrar);
+        cadastrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(LoginActivity.this,CadastroActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
 
     }
 }
