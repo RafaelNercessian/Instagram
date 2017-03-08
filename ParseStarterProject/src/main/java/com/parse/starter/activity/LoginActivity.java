9a655fb5ee5095c523da84d3844a882f0a63457e
RafaelNercessian <rafanercessian@gmail.com>
2017-03-07 22:46:42 -0300

    Fazendo login

--- ParseStarterProject/src/main/java/com/parse/starter/activity/LoginActivity.java
+++ ParseStarterProject/src/main/java/com/parse/starter/activity/LoginActivity.java
@@ -4,25 +4,45 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.parse.LogInCallback;
+import com.parse.Parse;
+import com.parse.ParseException;
 import com.parse.ParseUser;
 import com.parse.starter.R;
+import com.parse.starter.model.Usuario;
 
 public class LoginActivity extends AppCompatActivity {
 
     private TextView cadastrar;
+    private EditText usuario;
+    private EditText senha;
+    private Button botao;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        if(ParseUser.getCurrentUser()!=null){
-            Intent intent = new Intent(this,MainActivity.class);
+        usuario=(EditText) findViewById(R.id.login_usuario);
+        senha=(EditText) findViewById(R.id.login_senha);
+        botao=(Button) findViewById(R.id.login_botao);
+
+        if (ParseUser.getCurrentUser() != null) {
+            Intent intent = new Intent(this, MainActivity.class);
             startActivity(intent);
             finish();
-        }else {
+        } else {
+            botao.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    chamaMetodoParaLogar();
+                }
+            });
 
             cadastrar = (TextView) findViewById(R.id.login_cadastrar);
             cadastrar.setOnClickListener(new View.OnClickListener() {
@@ -34,5 +54,19 @@ public class LoginActivity extends AppCompatActivity {
                 }
             });
         }
+    }
+
+    private void chamaMetodoParaLogar() {
+        ParseUser.logInInBackground(usuario.getText().toString(), senha.getText().toString(), new LogInCallback() {
+            @Override
+            public void done(ParseUser user, ParseException e) {
+                    if(e==null){
+                        Toast.makeText(LoginActivity.this, "Usuário logado", Toast.LENGTH_SHORT).show();
+                    }else{
+                        e.printStackTrace();
+                        Toast.makeText(LoginActivity.this, "Erro", Toast.LENGTH_SHORT).show();
+                    }
+                }
+        });
     }
 }
