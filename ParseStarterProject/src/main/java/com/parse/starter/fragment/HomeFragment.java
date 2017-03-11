950fe3c2ac4f1b9741ca0c2d7cd9296d4d8f36a7
RafaelNercessian <rafanercessian@gmail.com>
2017-03-10 22:42:35 -0300

    Customizando ListView imagens

--- ParseStarterProject/src/main/java/com/parse/starter/fragment/HomeFragment.java
+++ ParseStarterProject/src/main/java/com/parse/starter/fragment/HomeFragment.java
@@ -6,13 +6,30 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
+import com.parse.FindCallback;
+import com.parse.ParseException;
+import com.parse.ParseObject;
+import com.parse.ParseQuery;
+import com.parse.ParseUser;
 import com.parse.starter.R;
+import com.parse.starter.adapter.HomeAdapater;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class HomeFragment extends Fragment {
+
+
+    private ListView listView;
+    private List<ParseObject> postagens;
+    private ArrayAdapter<ParseObject> adapter;
+    private ParseQuery<ParseObject> query;
 
 
     public HomeFragment() {
@@ -23,8 +40,37 @@ public class HomeFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+        listView=(ListView) view.findViewById(R.id.list_postagens_home);
+        postagens=new ArrayList<>();
+        HomeAdapater adapter = new HomeAdapater(getActivity(),postagens );
+        listView.setAdapter(adapter);
+
+        getPostagens();
+
+        return view;
+
     }
 
+    public void getPostagens() {
+        query = ParseQuery.getQuery("Imagem");
+        query.whereEqualTo("username", ParseUser.getCurrentUser().getUsername());
+        query.orderByAscending("createdAt");
+        query.findInBackground(new FindCallback<ParseObject>() {
+            @Override
+            public void done(List<ParseObject> objects, ParseException e) {
+                if(e==null){
+                    if(objects.size()>0){
+                        postagens.clear();
+                        for (ParseObject parseObject:objects ) {
+                            postagens.add(parseObject);
+                        }
+                        adapter.notifyDataSetChanged();
+                    }
+                }else{
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
 }
